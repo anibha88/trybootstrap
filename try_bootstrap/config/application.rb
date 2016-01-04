ef42560a93bdd0484d6fcb75ebd2bf92af267943
Anjana <anjana@Anjanas-MacBook-Pro.local>
2016-01-04 19:57:47 +0530

    Bootstrap page

--- try_bootstrap/config/application.rb
+++ try_bootstrap/config/application.rb
@@ -21,6 +21,7 @@ module TryBootstrap
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.assets.paths << "#{Rails}/assets/fonts"
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
